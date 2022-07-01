dbf812beb6a8eee964acf56f432f8c231ee82e4a
Bruce Alberto <brucealberto2@gmail.com>
2022-07-01 16:32:56 -0300

    files delted

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 
 app.listen(PORT, () => console.log(`SERVER LISTEN ON PORT ${PORT}`));
